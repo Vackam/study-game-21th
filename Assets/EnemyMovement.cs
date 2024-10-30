17c0cc167bbdb76426f796a22d26b1beed93edbf
Vackam <pay2889@gmail.com>
2024-10-30 20:27:37 +0900

    에셋변경 및 무기 3가지 구현
    에셋을 변경하였습니다.
    무기 3가지를 구현하였습니다.
    1. 직선형 무기
    2. 원형으로 도는 무기
    3. 폭탄형 무기
    
    시간 정지를 구현하였습니다.

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform playerTransform;
+    private float speed = 2.5f;
+    public float hp = 100;
+    void Start()
+    {
+        GameObject player = GameObject.FindWithTag("Player"); 
+        if(player != null)
+        {
+            playerTransform = player.transform;
+        }
+        else
+        {
+            Debug.LogError("Not Player Tag");
+        }
+    }
+
+    void DeleteEnemy()
+    {
+        if(this.hp <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DeleteEnemy();
+        if(playerTransform != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
+        }
+    }
+}
