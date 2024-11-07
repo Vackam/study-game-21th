9cc176514d410cb72795f5469322feace60e1793
Vackam <53655740+Vackam@users.noreply.github.com>
2024-11-07 14:44:45 +0900

    WeaponManger 생성
    딕셔너리로 관리.
    false일 시 작동 중지되게 구현해야함.

--- Assets/studyAsset/script/WeaponManager.cs
+++ Assets/studyAsset/script/WeaponManager.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class WeaponManager : MonoBehaviour
 {
+    static WeaponManager _instance;
+    public static WeaponManager Instance
+    {
+        get
+        {
+            if(_instance == null){}
+            return _instance;
+        }
+    }
     // Start is called before the first frame update
+    [SerializeField]
+    private List<Dictionary<bool, Weapon>> WeaponList = new List<Dictionary<bool, Weapon>>();
+
+    public AddWeapon(Weapon weapon)
+    {
+        Dictionary<bool, Weapon> weaponEntry = new Dictionary<bool, Weapon>();
+        weaponEntry.Add(true, weapon);
+    }
+
     void Start()
     {
-        
+        // 모든 무기는 사용자 중심으로 나가니까 하나 받아오기
+        GameObject player = GameMgr.Instance.GetPlayer();
     }
 
     // Update is called once per frame
