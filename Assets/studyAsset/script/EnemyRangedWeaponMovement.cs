3582ed4b97c137ce3f353bd2ddc1a93d6000e801
Vackam <pay2889@gmail.com>
2024-11-14 18:06:35 +0900

    원거리 적 구현

---
+++ Assets/studyAsset/script/EnemyRangedWeaponMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRangedWeaponMovement : MonoBehaviour
+{
+    private GameObject player;
+    private Vector3 ThatPosition;
+    private float range = 15.0f;
+    private float speed = 3.0f;
+
+    IEnumerator Delete()
+    {
+        yield return new WaitForSeconds(range);
+        Destroy(gameObject);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameMgr.Instance.GetPlayer();
+        ThatPosition = player.transform.position;
+        StartCoroutine(Delete());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, ThatPosition, speed * Time.deltaTime);
+    }
+}
